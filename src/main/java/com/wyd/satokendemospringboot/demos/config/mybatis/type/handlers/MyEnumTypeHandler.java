b1cfc0e84a7e026dc997664d57cbabc392281572
Stone-WYD <1174872613@qq.com>
2023-06-25 18:17:21 +0800

    优化mybatis转换类

--- src/main/java/com/wyd/satokendemospringboot/demos/config/mybatis/type/handlers/MyEnumTypeHandler.java
+++ src/main/java/com/wyd/satokendemospringboot/demos/config/mybatis/type/handlers/MyEnumTypeHandler.java
@@ -3,7 +3,6 @@ package com.wyd.satokendemospringboot.demos.config.mybatis.type.handlers;
 import com.wyd.satokendemospringboot.demos.constants.IEnum;
 import com.wyd.satokendemospringboot.demos.constants.TestIEum;
 import org.apache.ibatis.type.BaseTypeHandler;
-import org.apache.ibatis.type.EnumOrdinalTypeHandler;
 import org.apache.ibatis.type.JdbcType;
 import org.apache.ibatis.type.MappedTypes;
 
@@ -13,11 +12,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @MappedTypes(value = {TestIEum.class})
-public class MyEnumTypeHandler<E extends Enum> extends BaseTypeHandler<E> {
-    private final Class<E> type;
-    private final E[] enums;
+public class MyEnumTypeHandler extends BaseTypeHandler<IEnum> {
+    private final Class<IEnum> type;
+    private final IEnum[] enums;
 
-    public MyEnumTypeHandler(Class<E> type) {
+    public MyEnumTypeHandler(Class<IEnum> type) {
         if (type == null) {
             throw new IllegalArgumentException("Type argument cannot be null");
         } else {
@@ -32,32 +31,30 @@ public class MyEnumTypeHandler<E extends Enum> extends BaseTypeHandler<E> {
         }
     }
 
-    public void setNonNullParameter(PreparedStatement ps, int i, E parameter, JdbcType jdbcType) throws SQLException {
+    public void setNonNullParameter(PreparedStatement ps, int i, IEnum parameter, JdbcType jdbcType) throws SQLException {
         // 将枚举类中的字典值（value）sql语句中
-        IEnum iEnum = (IEnum) parameter;
-        Integer ordinal = iEnum.getCode();
+        Integer ordinal = parameter.getCode();
         ps.setInt(i, ordinal);
     }
 
-    public E getNullableResult(ResultSet rs, String columnName) throws SQLException {
+    public IEnum getNullableResult(ResultSet rs, String columnName) throws SQLException {
         int ordinal = rs.getInt(columnName);
         return ordinal == 0 && rs.wasNull() ? null : this.toOrdinalEnum(ordinal);
     }
 
-    public E getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+    public IEnum getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
         int ordinal = rs.getInt(columnIndex);
         return ordinal == 0 && rs.wasNull() ? null : this.toOrdinalEnum(ordinal);
     }
 
-    public E getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+    public IEnum getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
         int ordinal = cs.getInt(columnIndex);
         return ordinal == 0 && cs.wasNull() ? null : this.toOrdinalEnum(ordinal);
     }
 
-    private E toOrdinalEnum(int ordinal) {
-        for (E e : this.enums) {
-            IEnum ie = (IEnum) e;
-            if (ie.getCode() == ordinal) return e;
+    private IEnum toOrdinalEnum(int ordinal) {
+        for (IEnum e : this.enums) {
+            if (e.getCode() == ordinal) return e;
         }
         // 如果没找到字典值（value）对应的枚举类，则报错
         throw new IllegalArgumentException("Cannot convert " + ordinal + " to " + this.type.getSimpleName() + " by ordinal value.");
